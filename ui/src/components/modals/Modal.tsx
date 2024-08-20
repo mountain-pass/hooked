938bd51b023b2f891df8e4c0bff847d2e99ae972
Nick Grealy ⚡️ <nickgrealy@gmail.com>
2024-08-21 00:47:27 +1000

    Fixed modal.

--- ui/src/components/modals/Modal.tsx
+++ ui/src/components/modals/Modal.tsx
@@ -31,7 +31,7 @@ export const Modal = ({
     const Child = children
     return (
         <div
-            className={`animate-fade-in-out ${show ? 'show' : ''} flex items-start justify-center w-full h-full absolute top-0 left-0 right-0 bottom-0 backdrop-filter backdrop-blur-md`}
+            className={`animate-fade-in-out ${show ? 'show' : ''} flex items-start justify-center w-full h-full fixed top-0 left-0 right-0 bottom-0 backdrop-filter backdrop-blur-md`}
             onClick={() => enableBackgroundClose && setShow(false)}
         >
             <div className={`flex flex-col items-center justify-center p-4 w-full max-w-[500px] max-h-full ${className}`} onClick={(e) => e.stopPropagation()}>
